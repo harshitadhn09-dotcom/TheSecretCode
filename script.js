52176b5f49b0da17a6de16fd2f44e4873d3712e1
harshitadhn09-dotcom <harshitadhn09@gmail.com>
2026-06-22 14:07:56 +0530

    added logic

---
+++ script.js
@@ -0,0 +1,29 @@
+let secretCode = Math.floor(Math.random() * 100) + 1;//creates random number
+
+let attempts = 0;// counts attempts
+
+function checkGuess(){
+
+    let guess = Number(document.getElementById("guessInput").value);
+
+    attempts++;
+
+    document.getElementById("attempts").textContent =
+    "Transmission Attempts: " + attempts;
+
+    let missionLog = document.getElementById("missionLog");
+
+    if (guess === secretCode) {
+    missionLog.textContent = "ACCESS GRANTED. CODE VERIFIED.";}
+
+    else if (guess > secretCode) {
+    missionLog.textContent = "CODE EXCEEDS TARGET VALUE.";
+    }
+
+    else {
+    missionLog.textContent = "CODE BELOW TARGET VALUE.";    
+    }
+
+}
+
+    
